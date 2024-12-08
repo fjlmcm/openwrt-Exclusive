d41fbd71de8736d542c0cb0a9c6ab04c731dd0bc
fjlmcm <63480841+fjlmcm@users.noreply.github.com>
2024-12-08 14:29:02 +0800

    Update diy6.6.sh

--- diy6.6.sh
+++ diy6.6.sh
@@ -7,12 +7,20 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+
+# 修改默认IP
+# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+ sed -i '$i uci set network.lan.ifname="eth1"' package/lean/default-settings/files/zzz-default-settings
+ sed -i '$i uci set network.wan.ifname="eth0"' package/lean/default-settings/files/zzz-default-settings
+ sed -i '$i uci set network.wan.proto=pppoe' package/lean/default-settings/files/zzz-default-settings
+ sed -i '$i uci set network.wan6.ifname="eth0"' package/lean/default-settings/files/zzz-default-settings
+ sed -i '$i uci commit network' package/lean/default-settings/files/zzz-default-settings
+
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 sed -i '/openwrt-23.05/d' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 # sed -i '2i src-git luci https://github.com/coolsnowwolf/luci.git' feeds.conf.default
-
 
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
@@ -28,28 +36,22 @@ function drop_package(){
 
 rm -rf package/custom; mkdir package/custom
 
-# Add a feed source
-# git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
-# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-# git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
-# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/firkerword/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 chmod 755 ./package/lucky/luci-app-lucky/root/usr/bin/luckyarch
-git clone https://github.com/liudf0716/luci-app-apfree-wifidog.git package/wifidog
